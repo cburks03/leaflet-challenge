1c7f76f14a99df534dfa53cb29e9dbd93fd2709c
cburks03 <51494863+cburks03@users.noreply.github.com>
2020-02-05 19:52:23 -0500

    Add files via upload

---
+++ Leaflet-Step-1/config.js
@@ -0,0 +1,1 @@
+apiKey = "pk.eyJ1IjoiY2J1cmtzMDMiLCJhIjoiY2s0YWlzcW5nMDRmbjN1bncwMXZ0dnd1aCJ9.CU1XjhRmpL_T1qSIG7I_8A"
---
+++ Leaflet-Step-1/logic.js
@@ -0,0 +1,98 @@
+console.log("working");
+
+var apiKey = "pk.eyJ1IjoiY2J1cmtzMDMiLCJhIjoiY2s0YWlzcW5nMDRmbjN1bncwMXZ0dnd1aCJ9.CU1XjhRmpL_T1qSIG7I_8A";
+
+var graymap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 15,
+  id: "mapbox.streets",
+  accessToken: apiKey
+});
+
+var map = L.map("mapid", {
+  center: [
+    40, -100
+  ],
+  zoom: 4
+});
+
+graymap.addTo(map);
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_month.geojson", function(data) {
+
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: Color(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  function Color(magnitude) {
+    if (magnitude > 5) {
+        return 'red'
+    } else if (magnitude > 4) {
+        return 'darkorange'
+    } else if (magnitude > 3) {
+        return 'tan'
+    } else if (magnitude > 2) {
+        return 'yellow'
+    } else if (magnitude > 1) {
+        return 'darkgreen'
+    } else {
+        return 'lightgreen'
+    }
+  }
+
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+
+    return magnitude * 4;
+  }
+
+  L.geoJson(data, {
+  
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+  }).addTo(map);
+
+
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+  legend.onAdd = function() {
+    var div = L.DomUtil.create("div", "info legend");
+
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        "<i style='background: " + colors[i] + "'></i> " +
+        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+  legend.addTo(map);
+});
